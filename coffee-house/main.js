4ae9942afb33c6c1830eb275ae502e8d1a0bc320
tanyushanel <tatyana.nelipovich@gmail.com>
2023-12-08 21:36:07 +0100

    feat: added menu shifted right

--- coffee-house/main.js
+++ coffee-house/main.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const menu = document.getElementsByClassName("menu-container")[0];
+const button = document.getElementsByClassName("burger-menu-button")[0];
+
+function toggleMenu(event) {
+  event.preventDefault();
+
+  if (menu.classList.contains("show")) {
+    menu.classList.remove("show");
+    button.src = "assets/icons/button-icon-burger.svg";
+  } else {
+    menu.classList.add("show");
+    button.src = "assets/icons/button-icon-burger-close.svg";
+  }
+}
+
+button.addEventListener("touchstart", toggleMenu);
+button.addEventListener("click", toggleMenu);
