34c656caf6ad0f0519307348f309126d07f91a12
tanyushanel <tatyana.nelipovich@gmail.com>
2023-12-13 18:08:31 +0100

    fix: fixed refresh bttn

--- coffee-house/tabs.js
+++ coffee-house/tabs.js
@@ -61,12 +61,19 @@ function displayProducts(event, category, amount) {
     tabContent.appendChild(newProduct);
   });
 
+  if (shownProducts.length === filteredProducts.length) {
+    document.querySelector(".bttn-refresh").classList.add("hide");
+  } else {
+    document.querySelector(".bttn-refresh").classList.remove("hide");
+  }
+
   updateActiveTab(event);
 }
 
 function updateActiveTab(event) {
   const tabLinks = document.querySelectorAll(".tab-item");
   tabLinks.forEach((tab) => tab.classList.remove("active"));
+  console.log(event.currentTarget);
   event.currentTarget.classList.add("active");
 }
 
@@ -75,7 +82,9 @@ document.addEventListener("DOMContentLoaded", () => createTabs(products));
 function loadMore() {
   const tabContent = document.getElementById("tab-content");
   const shownProducts = tabContent.querySelectorAll(".grid-menu-item");
-  const category = document.querySelector(".active").innerText.toLowerCase();
+  const category = document
+    .querySelector(".tab-item.active")
+    .innerText.toLowerCase();
   const filteredProducts = products.filter(
     (product) => product.category === category
   );
@@ -86,22 +95,8 @@ function loadMore() {
   );
 
   hiddenProducts.forEach((product) => {
-    const productDiv = document.createElement("li");
-    productDiv.className = "grid-menu-item";
-    productDiv.innerHTML = `
-  
-      <div class="grid-item">
-          <img src="${product.src}" alt="pic" />
-      </div>
-      <div class="grid-item-desc margin-20 flex gap-12">
-          <h4>${product.name}</h4>
-          <p class="medium-txt cut">
-              ${product.description}
-          </p>
-          <h4 class="price">$${product.price}</h4>
-      </div>          
-          `;
-    tabContent.appendChild(productDiv);
+    const newProduct = createItemDiv(product);
+    tabContent.appendChild(newProduct);
   });
 
   if (!hiddenProducts.length) {
@@ -109,4 +104,4 @@ function loadMore() {
   }
 }
 
-document.addEventListener("touchend", loadMore);
+document.addEventListener("transitionend", loadMore);
